f43607ed7c6841aa88524200d3e7674a21b6fd9b
Luisa Eysele <eysele.luisa@gmail.com>
2023-08-18 11:07:21 -0600

    feat: revisión de código

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,22 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+const n = document.querySelector('.name'); // se quita el signo de $ a las variables y se corrige a referencia por clase
+const b = document.querySelector('.blog'); // se corrige a referencia por clase
+const l = document.querySelector('.location');
 
-function displayUser(username) {
-  $n.textContent = 'cargando...';
+async function displayUser(username) { // se añade el async para que el await funcione
+  n.textContent = 'cargando...'; // se elimina $
   const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  const data = await response.json(); // se crea variable data y se genera una respuesta json
+  n.textContent = `${data.name}`; // Se corrigen comillas para que lea las variables, se elimina $
+  b.textContent = `${data.blog}`; // Se corrigen comillas para que lea las variables, se elimina $
+  l.textContent = `${data.location}`; // Se corrigen comillas para que lea las variables, se elimina $
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  n.textContent = `Algo salió mal: ${err}` // se corrige la variable n
 }
 
 displayUser('stolinski').catch(handleError);
